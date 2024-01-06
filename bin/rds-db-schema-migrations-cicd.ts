108c28f0c6877fdcd01c288d91a89a34b8513193
Hossameldin Shahin <hosamlshahin@gmail.com>
2024-01-06 09:16:01 -0500

    Change input to github

--- bin/rds-db-schema-migrations-cicd.ts
+++ bin/rds-db-schema-migrations-cicd.ts
@@ -4,26 +4,11 @@ import { App } from '@aws-cdk/core';
 
 const app = new App();
 
-// environment variables set in the cdk-deploy-to script
-const envVariables = {
-  branch: process.env.BRANCH || 'main',
-  developmentAccount: safelyRetrieveEnvVariable('CDK_DEVELOPMENT_ACCOUNT'),
-  productionAccount: safelyRetrieveEnvVariable('CDK_PRODUCTION_ACCOUNT'),
-  region: safelyRetrieveEnvVariable('REGION'),
-  repositoryName: safelyRetrieveEnvVariable('REPOSITORY_NAME')
-}
-
-new CdkpipelinesStack(app, 'CdkpipelinesStack', envVariables, {
-  env: { account: process.env.CDK_DEVELOPMENT_ACCOUNT, region: process.env.REGION },
+new CdkpipelinesStack(app, 'CdkpipelinesStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
 });
 
 app.synth();
-
-function safelyRetrieveEnvVariable(envName: string): string {
-  const variable = process.env[envName];
-  if (!variable) {
-    throw new Error(`The variable ${envName} is required as environment variable. 
-    Either provide the variables, or run the cdk-* scripts provided by this module`);
-  }
-  return variable;
-}
--- lib/cdkpipelines-stack.ts
+++ lib/cdkpipelines-stack.ts
@@ -1,40 +1,36 @@
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, CodeBuildStep, ManualApprovalStep } from "@aws-cdk/pipelines";
 import { PolicyStatement, Effect } from "@aws-cdk/aws-iam"
-import { Repository } from "@aws-cdk/aws-codecommit";
 import { CdkpipelinesStage } from "./cdkpipelines-stage"
-
-interface Enviornment {
-  branch: string,
-  developmentAccount: string,
-  productionAccount: string,
-  region: string,
-  repositoryName: string
-}
 
 /**
  * The stack that defines the application pipeline
  */
 export class CdkpipelinesStack extends Stack {
-  private readonly devStageName: string = 'Development';
-  private readonly prodStageName: string = 'Production';
 
-  constructor(scope: Construct, id: string, envVariables: Enviornment, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const repository = Repository.fromRepositoryName(this, 'Repo', envVariables.repositoryName);
-    const defaultBranch = envVariables.branch;
+    const config = this.node.tryGetContext("config")
+    const accounts = config['accounts']
+    const connectionArn = config['connection_arn']
+
+    const input = CodePipelineSource.connection(
+      `${config['githubOrg']}/${config['githubRepo']}`,
+      config['githubBranch'],
+      { connectionArn }
+    )
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'RDSSchemaMigrationDemo',
       crossAccountKeys: true,
       synth: new CodeBuildStep('Synth', {
-        input: CodePipelineSource.codeCommit(repository, defaultBranch),
+        input,
         env: {
-          'CDK_DEVELOPMENT_ACCOUNT': envVariables.developmentAccount,
-          'CDK_PRODUCTION_ACCOUNT': envVariables.productionAccount,
-          'REPOSITORY_NAME': envVariables.repositoryName,
-          'BRANCH': envVariables.branch,
+          'CDK_DEVELOPMENT_ACCOUNT': accounts['CICD_ACCOUNT_ID'],
+          'CDK_PRODUCTION_ACCOUNT': accounts['PRD_ACCOUNT_ID'],
+          'REPOSITORY_NAME': config['githubRepo'],
+          'BRANCH': config['githubBranch'],
         },
         commands: [
           'npm ci',
@@ -49,24 +45,29 @@ export class CdkpipelinesStack extends Stack {
     });
 
     // development stage
-    const dev = new CdkpipelinesStage(this, this.devStageName, false, envVariables.developmentAccount, {
-      env: { account: envVariables.developmentAccount, region: this.region }
+    const dev = new CdkpipelinesStage(this, 'dev', false, accounts['CICD_ACCOUNT_ID'], {
+      env: { account: accounts['CICD_ACCOUNT_ID'], region: this.region }
     });
+
     pipeline.addStage(dev, {
       post: [this.generateDatabaseSchemaMigration(dev, this.region, this.account)]
     });
 
     // production stage
-    const prod = new CdkpipelinesStage(this, this.prodStageName, true, envVariables.developmentAccount, {
-      env: { account: envVariables.productionAccount, region: this.region }
+    const prod = new CdkpipelinesStage(this, 'prod', true, accounts['CICD_ACCOUNT_ID'], {
+      env: {
+        account: accounts['PRD_ACCOUNT_ID'],
+        region: this.region
+      }
     });
+
     pipeline.addStage(prod, {
       pre: [
         new ManualApprovalStep('ManualApproval', {
           comment: "Approve deployment to production"
         })
       ],
-      post: [this.generateDatabaseSchemaMigration(prod, this.region, envVariables.productionAccount)]
+      post: [this.generateDatabaseSchemaMigration(prod, this.region, accounts['PRD_ACCOUNT_ID'])]
     });
   }
 
@@ -81,7 +82,7 @@ export class CdkpipelinesStack extends Stack {
       })
     ]
 
-    if (stage.stageName === this.prodStageName) {
+    if (stage.stageName === 'prod') {
       // assume cross account role if production environment
       buildCommands.push(
         `aws sts assume-role --role-arn arn:aws:iam::${account}:role/${stage.crossAccountLambdaInvokeRoleName} --role-session-name "CrossAccountSession" > credentials.json`,
